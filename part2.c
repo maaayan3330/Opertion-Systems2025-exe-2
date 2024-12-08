58ff98bf636425ac22387ded2bbd263d18e7855e
maayan3330 <maayan3330@gmail.com>
2024-12-08 02:02:19 +0200

    commit

---
+++ part2.c
@@ -0,0 +1,16 @@
+// Maayan Ifergan 212437453
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+
+
+
+
+    return 0;
+}
